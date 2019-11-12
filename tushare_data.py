918a28036318dc60940db50bba3b4b8455c45153
James Lee <jameslee529@163.com>
2019-11-12 13:12:25 +0800

    feat: 开始试验持久化（到sqlite数据库）中的代码

--- tushare_data.py
+++ tushare_data.py
@@ -1,7 +1,8 @@
 """ 下载tushare提供的股票数据
 """
-from typing import Tuple
+from typing import Tuple, List
 from functools import reduce
+import sqlite3
 
 import tushare as ts
 import pandas as pd
@@ -18,5 +19,27 @@ def download_list_companies() -> Download_Result:
     return reduce(lambda x, y: x.append(y, ignore_index=True), list_companies), 3
 
 
+def persist_list_companies_to_db(list_companies: List):
+    conn = sqlite3.connect('example.db')
+    c = conn.cursor()
+
+    # Create table
+    c.execute('''CREATE TABLE list (代码 text, 名称 text, 地域 text, 行业 text, 上市日期 text, 退市日期 text)''')
+
+    # Insert a row of data
+    c.executemany('INSERT INTO list VALUES (?,?,?,?,?,?)', list_companies)
+
+    # Save (commit) the changes
+    conn.commit()
+
+    # We can also close the connection if we are done with it.
+    # Just be sure any changes have been committed or they will be lost.
+    conn.close()
+
+
+def transfer_list_companies(list_companies: pd.DataFrame) -> List:
+    return list_companies[0].values.tolist()
+
+
 if __name__ == '__main__':
-    print(download_list_companies())
+    persist_list_companies_to_db(transfer_list_companies(download_list_companies()))
